3b70fbb71bf9f618e1a6628d6baa0bda459a2525
JeanP2501 <jeanydrogos@gmail.com>
2025-12-02 16:02:38 -0500

    feat: eureka client implementation

--- src/main/java/com/bank/credit/CreditApplication.java
+++ src/main/java/com/bank/credit/CreditApplication.java
@@ -2,12 +2,14 @@ package com.bank.credit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main application class for Credit Service
  * Manages credit products (active products)
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CreditApplication {
 
 	public static void main(String[] args) {
